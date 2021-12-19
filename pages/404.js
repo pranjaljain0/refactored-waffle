4b6ba564620f9994e8d90705e340e14d7a7b2983
Pranjal Jain <pranjaljain0697@gmail.com>
2021-12-18 22:42:22 -0500

    Added 404 pages

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import styles from '../styles/404.module.css';
+
+export default function Custom404() {
+	return (
+		<div className={styles.ErrorHome}>
+			<h1>404-Page not found</h1>
+		</div>
+	);
+}
