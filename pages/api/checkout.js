437ce7e159ea4fb020c8e4019c58bc2d135011a9
Pranjal Jain <pranjaljain0697@gmail.com>
2021-12-23 06:56:14 -0500

    Added Backend for Checkout and Search

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,10 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { MongoClient } = require('mongodb');
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 	if (req.method !== 'POST') {
 		res.status(400).send({ message: 'Only POST requests allowed' });
 		return;
 	}
-	const body = JSON.parse(req.body);
-	console.log(`❗ checkout.js:9 'body'`, body);
+	const body = req.body;
+	if (!process.env.USERNAME)
+		throw new Error('Missing environment variable USERNAME');
+	if (!process.env.PASSWORD)
+		throw new Error('Missing environment variable PASSWORD');
+	if (!process.env.CLUSTURE)
+		throw new Error('Missing environment variable CLUSTURE');
+	if (!process.env.DATABASE)
+		throw new Error('Missing environment variable DATABASE');
+
+	const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTURE}.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+	const client = new MongoClient(uri);
+
+	try {
+		await client.connect();
+		const database = client.db('Waffles');
+		const collection = database.collection('Orders');
+
+		const result = await collection.insertOne(body);
+		console.log(`A document was inserted with the _id: ${result.insertedId}`);
+	} catch (err) {
+		console.log(`❗ checkout.js:28 'err'`, err);
+	} finally {
+		await client.close();
+	}
 }
--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,10 +1,28 @@
+import { useContext, useState } from 'react';
+
 import CartItem from '../../components/Cart/CartItem';
 import { cartContext } from '../../context/state';
 import styles from '../../styles/checkout.module.css';
-import { useContext } from 'react';
 
 const index = () => {
 	const { cartState, setCartState } = useContext(cartContext);
+	const [postData, setPostData] = useState({});
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
+		fetch('/api/checkout', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ userData: postData, cartData: cartState }),
+		});
+	};
+
+	const onChangeHandler = (e) => {
+		setPostData({ ...postData, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div className={styles.checkoutContainer}>
 			<h1>Checkout</h1>
@@ -15,15 +33,45 @@ const index = () => {
 					})}
 				</div>
 				<div className={styles.rightContainer}>
-					<form>
+					<form onSubmit={(e) => onSubmitHandler(e)}>
 						<span>Billing Details</span>
-						<input type='text' placeholder='Full Name' />
-						<input type='text' placeholder='Email' />
-						<input type='text' placeholder='Street' />
-						<input type='text' placeholder='City' />
-						<input type='text' placeholder='State' />
-						<input type='text' placeholder='Zip Code' />
-						<input type='submit' value={'Checkout'} />
+						<input
+							type='text'
+							placeholder='Full Name'
+							name='name'
+							onChange={onChangeHandler}
+						/>
+						<input
+							type='text'
+							placeholder='Email'
+							name='email'
+							onChange={onChangeHandler}
+						/>
+						<input
+							type='text'
+							placeholder='Street'
+							name='street'
+							onChange={onChangeHandler}
+						/>
+						<input
+							type='text'
+							placeholder='City'
+							name='city'
+							onChange={onChangeHandler}
+						/>
+						<input
+							type='text'
+							placeholder='State'
+							name='state'
+							onChange={onChangeHandler}
+						/>
+						<input
+							type='text'
+							placeholder='Zip Code'
+							name='zip'
+							onChange={onChangeHandler}
+						/>
+						<input type='submit' value='Checkout' />
 					</form>
 				</div>
 			</div>
